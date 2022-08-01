0cbd22aeb63a6c4c4ea5c8413aa67da92fa0a1f9
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-08-01 11:35:34 +0200

    added bot track

---
+++ bot/Main.ts
@@ -0,0 +1,49 @@
+import {Client, type Member, type Guild} from "eris";
+import {config as dotenvConfig} from "dotenv";
+import Cache from "./Cache";
+import { resolve } from "path";
+
+const path = resolve(__dirname, "../", ".env");
+const envConfig = dotenvConfig({path});
+const ownerID = "331265944363991042";
+
+if (!envConfig?.parsed || envConfig.error) {
+  console.log(envConfig.error);
+  throw "Unable to open .env file";
+};
+
+const token = envConfig.parsed["TOKEN"];
+
+const bot = new Client(`Bot ${token}`, {
+  intents: ["guilds", "guildPresences", "guildMembers"],
+  getAllUsers: true
+});
+
+bot.on("ready", () => {
+  console.log("Bot: Connected.");
+});
+
+bot.on("guildCreate", (guild: Guild) => {
+  if (guild.id !== "861938470841745459") return;
+
+  let personnel = guild.members.get(ownerID);
+  if (personnel && personnel.activities?.length) {
+    return Cache.set(personnel.activities);
+  } else {
+    return;
+  };
+})
+
+bot.on("presenceUpdate", (member: Member, _) => {
+  // not creator itself, or any bot
+  if (member.bot || member.id !== ownerID) return;
+
+  // no activities
+  if (!member?.activities?.length) {
+    return Cache.reset();
+  };
+
+  return Cache.set(member.activities);
+});
+
+export default bot.connect();
